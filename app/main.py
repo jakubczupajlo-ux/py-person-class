8812dbd42cc929b1c870deb22b00414057debc68
Jakub <jakubczupajlo@gmail.com>
2025-12-28 20:52:29 +0100

    new version

--- app/main.py
+++ app/main.py
@@ -1,36 +1,21 @@
 class Person:
-    # Atrybut klasowy przechowujący wszystkie instancje
     people = {}
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        # Dodawanie instancji do słownika klasowego przy inicjalizacji
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # Faza 1: Tworzymy wszystkie instancje Person
-    # Dzięki temu Person.people zostanie wypełnione wszystkimi obiektami
-    for person_dict in people:
-        Person(person_dict["name"], person_dict["age"])
+    person_instances = [Person(p.get("name"), p.get("age")) for p in people]
 
-    result_list = []
+    for p_dict in people:
+        instance = Person.people.get(p_dict.get("name"))
+        spouse_name = p_dict.get("wife") or p_dict.get("husband")
+        spouse_key = "wife" if p_dict.get("wife") else "husband"
 
-    # Faza 2: Łączymy małżonków i budujemy listę wynikową
-    for person_dict in people:
-        name = person_dict["name"]
-        instance = Person.people[name]
-        
-        # Sprawdzamy, czy w słowniku jest klucz 'wife' lub 'husband'
-        spouse_key = "wife" if "wife" in person_dict else "husband"
-        spouse_name = person_dict.get(spouse_key)
+        if spouse_name:
+            setattr(instance, spouse_key, Person.people.get(spouse_name))
 
-        # Jeśli małżonek istnieje (nie jest None), przypisujemy link do obiektu
-        if spouse_name is not None:
-            # Używamy setattr, aby dynamicznie ustawić atrybut 'wife' lub 'husband'
-            setattr(instance, spouse_key, Person.people[spouse_name])
-        
-        result_list.append(instance)
-
-    return result_list
+    return person_instances
